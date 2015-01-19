ff03b49c0d21dbdfdffa8d2aa8fcdc738bc5f091
lovedeeply <lovedeeply008@126.com>
2015-01-19 17:59:50 +0800

    获取太平洋搜索的"5s 三网"最新帖子

--- TPY.py
+++ TPY.py
@@ -1,1 +1,29 @@
 __author__ = 'gaoguanghe'
+from bs4 import BeautifulSoup
+import os, sys, urllib2,time,random,string,Tkinter,tkMessageBox
+reload(sys)
+sys.setdefaultencoding('utf-8')
+global list
+list=[]
+flag = False
+def getNewTpy():
+    targetUrl = 'http://ks.pconline.com.cn/bbs.shtml?forumValue=240027&q=5s+%C8%FD%CD%F8&scope=all&pageNo=1'
+    content = urllib2.urlopen(targetUrl)
+    soup = BeautifulSoup(content,from_encoding='GB18030')
+    nav = soup.select('.iTimeIcon')
+    titles = soup.select(".blue")
+    for t in titles:
+        a = t["href"]+'\n'+t.text
+        if a not in list:
+            list.insert(0,a)
+            print(a)
+            if flag:
+                tkMessageBox.showinfo("",a)
+getNewTpy()
+flag=True
+while True:
+    time.sleep(60)
+    getNewTpy()
+    print("\n")
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+
